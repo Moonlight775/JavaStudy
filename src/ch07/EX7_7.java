509153b6db1042542dcd94abd12e91de79cc62c0
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-07 23:47:45 +0900

    push test

--- src/ch07/EX7_7.java
+++ src/ch07/EX7_7.java
@@ -11,7 +11,10 @@ public class EX7_7 {
 	//	car.water();	// car type은 water()를 사용할 수 없다.
 		fe2 = (FireEngine)car;	// 자손타입 <- 조상타입. 형변환 생략 불가
 		fe2.water();
-		//
+		
+//		Car c = new Car();
+//		FireEngine fe = (FireEngine)c; // 형변환 실행 에러 java.lang.ClassCastException
+//		fe.wtaer(); //컴파일 오케이
 	}
 }
 
